f156e7e814b5236c0af4bb5ee936beee79608f33
JL-OLEMAR <joseluis19963@gmail.com>
2021-09-19 20:20:46 -0500

    Add Rutas hijas y Lazyload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+
+import { ErrorPageComponent } from './shared/error-page/error-page.component'
+
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: async () => (
+      await import('./auth/auth.module').then(m => m.AuthModule)
+    )
+  },
+  {
+    path: 'heroes',
+    loadChildren: async () => (
+      await import('./heroes/heroes.module').then(m => m.HeroesModule)
+    )
+  },
+  { path: '404', component: ErrorPageComponent },
+  { path: '**', redirectTo: '404' }
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
---
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html',
+  styles: [
+  ]
+})
+export class HeroeComponent implements OnInit {
+  // constructor () { }
+
+  ngOnInit (): void {
+  }
+}
