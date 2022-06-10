3cc1b60daeaf7de5e001e0abc9afa4a26835ff34
José Olemar <joseluis19963@gmail.com>
2022-06-09 21:23:55 -0500

    Revert "Revert "Crud""

--- src/app/heroes/pipes/imagen.pipe.ts
+++ src/app/heroes/pipes/imagen.pipe.ts
@@ -1,15 +1,19 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import { Pipe, PipeTransform } from '@angular/core'
 import { Heroe } from '../interfaces/heroes.interface'
 
 @Pipe({
   name: 'imagen'
+  // pure: false // para renderizar en cada cambio
 })
 export class ImagenPipe implements PipeTransform {
   transform (heroe: Heroe): string {
-    if (heroe.alt_img === undefined) {
-      return `assets/heroes/${heroe.id}.jpg` // eslint-disable-line
-    } else {
+    if (!heroe.id && !heroe.alt_img) {
       return 'assets/no-image.png'
+    } else if (heroe.alt_img) {
+      return heroe.alt_img
+    } else {
+      return `assets/heroes/${heroe.id}.jpg` // eslint-disable-line
     }
   }
 }
